c7ff5e4cc89fe2695d8726d26e77aa5828983421
Yulian302 <bohomolyulian3022003@gmail.com>
2023-05-10 21:08:04 +0300

    Products visualization from postgres db

--- backend/routes/rest_routes.py
+++ backend/routes/rest_routes.py
@@ -1,10 +1,13 @@
 from app import app
 from flask import redirect,url_for
+from models.Product import Product
+from flask import jsonify
 
 @app.route('/')
 def initial():
     return redirect(url_for('home',__external=True))
 
 @app.route('/home')
-def home(): 
-    return 'home'
+def home():
+    products = Product.query.all()
+    return jsonify([{'id':product.id,'name':product.name,'price':product.price,'img_url':product.img_url,'description':product.description} for product in products])
